f9a55f0eb33fff031cd602cce05bc394d021b6d9
Shubham7204 <onlinelearner01learn@gmail.com>
2024-03-19 00:54:53 +0530

    SparkX

---
+++ src/components/Feed.jsx
@@ -0,0 +1,42 @@
+import { SideBar, Videos } from './';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('Education');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => {setVideos(data.items)})
+  }, [selectedCategory]);
+
+  return (
+    <Stack
+      sx = {{flexDirection: {sx: 'column', md: 'row'}}}
+    >
+      <Box
+        sx = {{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}
+      >
+        <SideBar 
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
+      </Box>
+      
+      <Box pl = {2} sx = {{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant = "h4" fontWeight = "bold" mb = {2} sx = {{color: 'white'}}>
+          {selectedCategory} <span style = {{color: 'blue'}}>
+            Videos
+          </span>
+        </Typography>
+
+        <Videos videos = {videos}/>
+      </Box>
+
+    </Stack>
+  )
+}
+
+export default Feed
---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SchoolIcon from '@mui/icons-material/School';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Education', icon: <HomeIcon />, },
+  { name: 'Class 9-10', icon: <SchoolIcon />, },
+  { name: 'Class 11-12', icon: <SchoolIcon />, },
+  { name: 'Comp Science', icon: <CodeIcon /> },
+  { name: 'Physics', icon: <SchoolIcon />, },
+  { name: 'Chemistry', icon: <SchoolIcon />, },
+  { name: 'Mathematics', icon: <SchoolIcon />, },
+  { name: 'JEE', icon: <SchoolIcon />, },
+  { name: 'UPSC', icon: <SchoolIcon />, },
+  { name: 'NEET', icon: <SchoolIcon />, },
+  { name: 'Web Dev', icon: <SchoolIcon />, },
+  { name: 'Blockchain', icon: <DeveloperModeIcon />, },
+  { name: 'Data Science', icon: <OndemandVideoIcon />, },
+  { name: 'Cloud Computing', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
